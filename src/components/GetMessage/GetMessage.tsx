c483ab5099270bbc0f7063f3d7643a4195f7e73c
Nurzat Taalaibekova <tnurzat91@gmail.com>
2023-11-20 14:32:51 +0300

    changing the display of messages on the page

--- src/components/GetMessage/GetMessage.tsx
+++ src/components/GetMessage/GetMessage.tsx
@@ -1,15 +1,18 @@
 import GetListForm from '../GetListForm/GetListForm';
 import { useEffect, useState } from 'react';
 import { SendProps } from '../../types';
+import './Loader.css';
 
 const GetMessage = () => {
   const [listItem, setListItem] = useState<SendProps[]>([]);
   const [lastDatetime, setLastDatetime] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const url = 'http://146.185.154.90:8000/messages';
 
   useEffect(() => {
     const fetchMessage = async () => {
       try {
+        setIsLoading(true);
         const response = await fetch(url);
         if (!response.ok) {
           throw new Error('ERROR ' + response.status);
@@ -18,29 +21,51 @@ const GetMessage = () => {
         const posts: SendProps[] = await response.json();
 
         if (posts.length > 0) {
-          setListItem((prev) => [...prev, ...posts]);
           const latestDatetime = posts[posts.length - 1].datetime;
-          setLastDatetime(latestDatetime);
+          if (latestDatetime !== lastDatetime) {
+            setListItem(posts);
+            setLastDatetime(latestDatetime);
+          }
         }
-        const dataresponse = await fetch(`${url}?datetime=${lastDatetime}`);
-        const dataMessage: SendProps[] = await dataresponse.json();
 
-        setListItem(dataMessage.reverse());
+      } catch (error) {
+        console.error('Error:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    const fetchMessageLastDatetime = async () => {
+      try {
+        const dataresponse = await fetch(`${url}?datetime=${lastDatetime}`);
+        if (!dataresponse.ok) {
+          throw new Error('ERROR ' + dataresponse.status);
+        }
+        const dataMessage: SendProps[] = await dataresponse.json();
+        if (dataMessage.length > 0) {
+          setListItem(dataMessage.reverse());
+        }
       } catch (error) {
         console.error('Error:', error);
       }
     };
 
     void fetchMessage();
-    const  interval = setInterval(fetchMessage, 3000);
+    const interval = setInterval(fetchMessageLastDatetime, 3000);
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div>
-      <GetListForm listItem={listItem} />
-    </div>
+    <>
+      {isLoading ? (
+        <div className="loader">Loading...</div>
+      ) : (
+        <div>
+          <GetListForm listItem={listItem} />
+        </div>
+      )}
+    </>
   );
 };
 
